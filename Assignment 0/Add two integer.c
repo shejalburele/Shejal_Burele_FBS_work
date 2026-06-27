1340bdbe3f07c47ec2ff9825201738ff0061d668
Shejal Burele <shejalburele2005@gmail.com>
2026-06-27 18:59:41 +0530

    Add files via upload
    Add Assignment 0 folder

---
+++ Assignment 0/Add two integer.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int a=-333,b=256,add;
+	add=a+b;
+	printf("The Addition of Two Integer is:%d",add);
+}
---
+++ Assignment 0/area of triangle.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	float base=5,height=15,area;
+	area=(base*height)/2;
+	printf("The Area of triangle is %f",area);
+}
