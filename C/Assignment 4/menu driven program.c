0b401638d3547a8868fcce0d827833692764248d
Shejal Burele <shejalburele2005@gmail.com>
2026-07-14 19:38:47 +0530

    Add files via upload

---
+++ C/Assignment 4/menu driven program.c
@@ -0,0 +1,94 @@
+//menu driven program to perform operations
+#include<stdio.h>
+void main()
+{
+	int num,choice;
+	printf("enter the number \n");
+	scanf("%d",&num);
+	printf("press the choice to perform operations \n");
+	printf("1.To check number is even or odd. \n");
+	printf("2.To check number is prime or not.\n");
+	printf("3.To check number is pallindrome or not. \n");
+	printf("4.To check number is positive, negative or zero.\n");
+	printf("5.To reverse a number. \n");
+	printf("6.To find sum of digits. \n");
+	scanf("%d",&choice);
+	
+	if(choice==1)
+	{
+		if(num%2==0)
+		    printf("%d is even",num);
+		else
+		    printf("%d is odd",num);
+	}
+	
+	else if(choice==2)
+	{
+		int prime=0;
+		for(int i=2;i<num;i++)
+		{
+			if(num%i==0)
+			    prime=1;
+		}
+		if(prime==0)
+		    printf("%d is prime number",num);
+		else
+		    printf("%d is not prime number",num);
+	}
+	
+	else if(choice==3)
+	{
+		int tem=num;
+		int rem,rev=0;
+		while(tem>0)
+		{
+			rem=tem%10;
+			rev=rev*10+rem;
+			tem=tem/10;
+		}
+		if(rev==num)
+		    printf("%d is pallindrome",num);
+		else
+		    printf("%d is not palindrome",num);
+	}
+	
+	else if(choice==4)
+	{
+		if(num==0)
+		    printf("number is zero");
+		else if(num>0)
+		    printf("number is positive");
+		else if(num<0)
+		    printf("number is negative");
+		else
+		    printf("invalid number");
+	}
+	
+	else if(choice==5)
+	{
+		int tem=num;
+		int rev=0,rem;
+		while(tem>0)
+		{
+			rem=tem%10;
+			rev=rev*10+rem;
+			tem=tem/10;
+		}
+		printf("the reverse of %d is:%d",num,rev);
+	}
+	
+	else if(choice==6)
+	{
+		int tem=num;
+		int rem,sum=0;
+		while(tem>0)
+		{
+			rem=tem%10;
+			sum=sum+rem;
+			tem=tem/10;
+		}
+		printf("the sum of digits is %d",sum);
+	}
+	else
+	    printf("invalid choice");
+}
